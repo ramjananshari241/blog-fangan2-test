ab77717ac5bb08302bf2c459460fb9292e91537c
ramjananshari241 <ramjananshari241@gmail.com>
2026-01-04 06:47:48 +0800

    Update blog.config.ts

--- blog.config.ts
+++ blog.config.ts
@@ -77,7 +77,7 @@ interface BlogConfig {
 
 const CONFIG: BlogConfig = {
   NOTION_PAGE_ID:
-    process.env.NOTION_PAGE_ID ?? '2d5cb1e3b03e81539638ec08f21a0892',
+    process.env.NOTION_PAGE_ID ?? '2ddde8e9973c81f58262e0f64a566e50',
   NOTION_SITE_NAME: 'anzifan',
   NEXT_REVALIDATE_SECONDS: 60,
   FORCE_UPDATE_TIME: 1620000000000,
